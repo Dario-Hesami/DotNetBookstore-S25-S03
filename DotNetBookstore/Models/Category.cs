a004742833277c01b2e87dc2e53cb5acf1d0bfbe
Dario Hesami <hossein.hesami@georgiancollege.ca>
2025-05-30 14:01:48 -0400

    Week 4 - complete

--- DotNetBookstore/Models/Category.cs
+++ DotNetBookstore/Models/Category.cs
@@ -3,11 +3,29 @@
 namespace DotNetBookstore.Models
 {
     public class Category
+
     {
-        //[Range(100, 500, ErrorMessage = "Please enter a valid category ID")]
-        public int CategoryId { get; set; }
-        [Display(Name = "Category Name")]
-        [Required(ErrorMessage = "Please enter a category name")]
-        public string Name { get; set; }
+
+        // PK
+
+        public int CategoryId
+        {
+            get; set;
+        }
+
+
+
+        [Required(ErrorMessage = "A customized error message")]
+
+        [MaxLength(100)]
+
+        public string Name { get; set; } = string.Empty;
+
+
+
+        // Navigation property: A category can have many books (optional from the category side)
+
+        public ICollection<Book> Books { get; set; } = [];
+
     }
 }
