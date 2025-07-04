b2cca0807d39c26e9581d9ccf2861b33acabcbbe
Dario Hesami <hossein.hesami@georgiancollege.ca>
2025-07-04 13:58:33 -0400

    Week 8 - Part 2 - Complete

---
+++ SQLQuery1.sql
@@ -0,0 +1,5 @@
+﻿INSERT INTO AspNetRoles (Id, Name, NormalizedName) VALUES ('1', 'Administrator', 'ADMINISTRATOR');
+
+INSERT INTO AspNetRoles (Id, Name, NormalizedName) VALUES ('2', 'Customer', 'CUSTOMER');
+
+INSERT INTO AspNetUserRoles (RoleId, UserId) (SELECT '1', Id FROM AspNetUsers);
