47e606bc324f356a02aa7963e8c12aa7144a76bd
Dario Hesami <hossein.hesami@georgiancollege.ca>
2025-07-11 13:48:32 -0400

    Week 9 - complete

--- DotNetBookstore/Program.cs
+++ DotNetBookstore/Program.cs
@@ -15,6 +15,16 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.Requ
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
+// enable Google Auth before our app starts
+// access the cinfig values in appsettings.json
+var configuration = builder.Configuration;
+builder.Services.AddAuthentication()
+    .AddGoogle(options =>
+    {
+        options.ClientId = configuration["Authentication:Google:ClientId"];
+        options.ClientSecret = configuration["Authentication:Google:ClientSecret"];
+    });
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
