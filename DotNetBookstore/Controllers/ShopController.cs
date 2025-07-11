0d3c35aeb4c6aa4d86dad539645f94257ea4cb3a
Dario Hesami <hossein.hesami@georgiancollege.ca>
2025-07-11 11:57:02 -0400

    Week 9 - Part 1 - ShopController

---
+++ DotNetBookstore/Controllers/ShopController.cs
@@ -0,0 +1,28 @@
+﻿using DotNetBookstore.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetBookstore.Controllers
+{
+    public class ShopController : Controller
+    {
+        // This controller is responsible for handling requests related to the shop
+        // class level DbContext connection object
+        // This is used to access the database
+        private readonly ApplicationDbContext _context;
+
+        // constructor that accepts a DbContext instance
+        // This allows dependency injection to provide the context
+        public ShopController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            // query the list of categories from the database in a-z order for display
+            var categories = _context.Categories
+                .OrderBy(c => c.Name)
+                .ToList();
+            return View(categories);
+        }
+    }
+}
